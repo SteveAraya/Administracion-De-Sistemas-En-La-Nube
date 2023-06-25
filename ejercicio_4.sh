41401ada2630006366999ee01765c2d729377a74
ABugFinder <bicho_suarez@hotmail.com>
2023-06-24 21:55:38 -0700

    Added script code for ejercicio_4
    Added first version of ejercicio_4, can be optimized and it's tested so can create copies. It is intended to create a copy from holaMundo.txt from testFiles directory.

--- ejercicio_4.sh
+++ ejercicio_4.sh
@@ -1,1 +1,23 @@
 echo "Ejercicio 4"
+
+# Escribid un script que copie un archivo sobre otro, garantizando que solo reciba dos parámetros.
+
+
+# Verificar que se proporcionen exactamente dos argumentos
+if [ $# -ne 2 ]; then # si el número de argumentos ($#) no es igual (-ne) a 2, entonces mostrar error
+  echo "Error: Se deben proporcionar exactamente dos parámetros."
+  exit 1
+fi
+
+origenFile="$1"
+destinoFile="$2"
+
+# Verificar si el archivo de origen existe
+if [ ! -f "$origenFile" ]; then # ! operador lógico de negación,
+  echo "Error: El archivo de origen '$origenFile' no existe."
+  exit 1
+fi
+
+# Copiar el archivo de origen al destino
+cp "$origenFile" "$destinoFile"
+echo "El archivo '$origenFile' ha sido copiado exitosamente a '$destinoFile'."
