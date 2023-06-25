ea4434b372b773d2eb6e32941ee7b2ca1aa0172d
Enrique Bazúa <enriquegbazua@gmail.com>
2023-06-24 23:31:46 -0700

    Segundo ejercicio

--- ejercicio_2.sh
+++ ejercicio_2.sh
@@ -1,1 +1,29 @@
-echo "Ejercicio 2"
+# Escribir un script que ejecute cierta acción en función de la extensión de un archivo
+# que recibe como parámetro. Si se trata de un JPG, copiar dicho archivo en la carpeta
+# ~/fotos. Si resulta ser de otro formato, avisar al usuario sin hacer nada.
+
+#!/bin/bash
+
+archivo="$1"   # El archivo se pasa como parámetro al script
+
+# Verificar si el archivo existe
+if [ -f "$archivo" ]; then
+  # Obtener la extensión del archivo
+  extension="${archivo##*.}"
+
+  # Verificar la extensión y ejecutar la acción correspondiente
+  case "$extension" in
+    jpg|JPG)
+      # Copiar el archivo en la carpeta ~/fotos
+      cp "$archivo" ~/fotos/
+      echo "Archivo copiado en ~/fotos."
+      ;;
+    *)
+      # Otro formato de archivo
+      echo "El archivo no es de formato JPG."
+      ;;
+  esac
+
+else
+  echo "El archivo no existe."
+fi
